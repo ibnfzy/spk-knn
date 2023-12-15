ea5b1c18fb4fdaf05e39322380bb5fede7713d67
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2023-12-15 23:59:12 +0800

    fix up

--- app/Views/guru/uji_add.php
+++ app/Views/guru/uji_add.php
@@ -13,55 +13,88 @@
         <select name="id_murid" id="id_murid" class="form-control">
           <option selected disabled>-- PILIH MURID --</option>
           <?php $i = 1;
-          foreach ($murid as $item): ?>
-            <option value="<?= $item['id_murid']; ?>">
-              <?= $i++ . '. ' . $item['nama_lengkap'] . ' | NIS. ' . $item['nis']; ?>
-            </option>
+          foreach ($murid as $item) : ?>
+          <option value="<?= $item['id_murid']; ?>">
+            <?= $i++ . '. ' . $item['nama_lengkap'] . ' | NIS. ' . $item['nis']; ?>
+          </option>
           <?php endforeach ?>
         </select>
       </div>
 
       <?php $db = \Config\Database::connect(); ?>
 
-      <?php foreach ($kriteria as $node): ?>
-        <hr>
+      <?php foreach ($kriteria as $node) : ?>
+      <hr>
 
-        <h5 class="font-weight-bold">
-          <?= $node['nama_kriteria']; ?>
-        </h5>
+      <h5 class="font-weight-bold">
+        <?= $node['nama_kriteria']; ?>
+      </h5>
 
-        <?php
-        $get = $db->table('kuisoner')->where('kriteria', $node['nama_kriteria'])->get()->getResultArray()
-        ;
+      <?php
+        $get = $db->table('kuisoner')->where('kriteria', $node['nama_kriteria'])->get()->getResultArray();
         $i = 1;
         ?>
 
-        <?php foreach ($get as $val): ?>
-          <div class="form-group col-6">
-            <label>
-              <?= $val['pertanyaan']; ?>
-            </label>
-            <select name="pertanyaan[<?= $node['nama_kriteria'] ?>][<?= $i ?>]" id="pertanyaan" class="form-control">
-              <?php if (fmod($i, 2) == 1): ?>
-                <option selected disabled>PILIH JAWABAN</option>
-                <option value="35">1. Sangat Setuju</option>
-                <option value="25">2. Setuju</option>
-                <option value="20">3. Netral</option>
-                <option value="15">4. Tidak Setuju</option>
-                <option value="5">5. Sangat Tidak Setuju</option>
-              <?php else: ?>
-                <option selected disabled>PILIH JAWABAN</option>
-                <option value="30">1. Sangat Setuju</option>
-                <option value="25">2. Setuju</option>
-                <option value="20">3. Netral</option>
-                <option value="15">4. Tidak Setuju</option>
-                <option value="10">5. Sangat Tidak Setuju</option>
-              <?php endif ?>
-            </select>
-          </div>
+      <?php foreach ($get as $val) : ?>
+      <div class="form-group col-6">
+        <label>
+          <?= $val['pertanyaan']; ?>
+        </label>
+        <select name="pertanyaan[<?= $node['nama_kriteria'] ?>][<?= $i ?>]" id="pertanyaan" class="form-control">
+          <?php if (str_contains($val['pertanyaan'], 'lancar')) : ?>
+          <?php if (fmod($i, 2) == 1) : ?>
+          <option selected disabled>PILIH JAWABAN</option>
+          <option value="35">1. Sangat Lancar</option>
+          <option value="25">2. Lancar</option>
+          <option value="20">3. Biasa</option>
+          <option value="15">4. Tidak Lancar</option>
+          <option value="5">5. Sangat Tidak Lancar</option>
+          <?php else : ?>
+          <option selected disabled>PILIH JAWABAN</option>
+          <option value="30">1. Sangat Lancar</option>
+          <option value="25">2. Lancar</option>
+          <option value="20">3. Biasa</option>
+          <option value="15">4. Tidak Lancar</option>
+          <option value="10">5. Sangat Tidak Lancar</option>
+          <?php endif ?>
+          <?php elseif (str_contains($val['pertanyaan'], 'dapat')) : ?>
+          <?php if (fmod($i, 2) == 1) : ?>
+          <option selected disabled>PILIH JAWABAN</option>
+          <option value="35">1. Sangat Bisa</option>
+          <option value="25">2. Bisa</option>
+          <option value="20">3. Cukup Bisa</option>
+          <option value="15">4. Kurang Bisa</option>
+          <option value="5">5. Tidak Bisa</option>
+          <?php else : ?>
+          <option selected disabled>PILIH JAWABAN</option>
+          <option value="30">1. Sangat Bisa</option>
+          <option value="25">2. Bisa</option>
+          <option value="20">3. Cukup Bisa</option>
+          <option value="15">4. Kurang Bisa</option>
+          <option value="10">5. Tidak Bisa</option>
+          <?php endif ?>
+          <?php else : ?>
+          <?php if (fmod($i, 2) == 1) : ?>
+          <option selected disabled>PILIH JAWABAN</option>
+          <option value="35">1. Sangat Setuju</option>
+          <option value="25">2. Setuju</option>
+          <option value="20">3. Netral</option>
+          <option value="15">4. Tidak Setuju</option>
+          <option value="5">5. Sangat Tidak Setuju</option>
+          <?php else : ?>
+          <option selected disabled>PILIH JAWABAN</option>
+          <option value="30">1. Sangat Setuju</option>
+          <option value="25">2. Setuju</option>
+          <option value="20">3. Netral</option>
+          <option value="15">4. Tidak Setuju</option>
+          <option value="10">5. Sangat Tidak Setuju</option>
+          <?php endif ?>
+          <?php endif ?>
+        </select>
+      </div>
 
-          <?php $i++; ?>
-        <?php endforeach ?>
+      <?php $i++; ?>
+      <?php endforeach ?>
       <?php endforeach ?>
     </div>
     <div class="card-footer">
